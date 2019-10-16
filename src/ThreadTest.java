f9ddf3ad1dfda892ba89a96cd13c1dce52ba659d
unknown <leonidb93@gmail.com>
2019-10-16 11:06:57 +0200

    practice with synchronized

--- src/ThreadTest.java
+++ src/ThreadTest.java
@@ -1,18 +1,47 @@
+import java.util.Vector;
+
+class Token {
+	volatile int counter = 0; 
+}
 
 public class ThreadTest implements Runnable{
 
+	Token obj;
+	
+	public ThreadTest(Token obj) {
+		this.obj = obj;
+	}
     @Override
     public void run() {
-        System.out.println("Inside : " + Thread.currentThread().getName());
         for (int i = 1; i <= 100; i++) {
-			System.out.println(i);
+        	if(i % 20 == 0) {
+        		synchronized (obj) {
+        			try {
+        				obj.counter++;
+        				if(obj.counter < 5-1) {
+        					obj.wait();
+        				}
+        				
+        				obj.counter = 0;
+        				obj.notifyAll();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+    			}
+        		
+        	}
+        	
+			System.out.println(i + " " + Thread.currentThread().getName());
 		}
     }
     
     
 	public static void main(String[] args) {
+		Token obj = new Token();
+		
 		for (int i = 0; i < 5; i++) {
-			Runnable runnable = new ThreadTest();
+			Runnable runnable = new ThreadTest(obj);
 	        Thread thread = new Thread(runnable);
 	        thread.start();
 		}
